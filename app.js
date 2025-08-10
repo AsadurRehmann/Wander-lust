e98f6b63f779f507e3b3e056ef10725d511f8df8
AsadurRehmann <asadurrehman886@gmail.com>
2025-08-10 08:19:39 +0500

    added most of the client side validations and server side listing validation is not FUCKING WORKING cz of some FUCKING UNDEFINED

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const expressError = require("./utils/expressError.js");
+const cors = require("cors"); //for hopscotch
+const Joi = require("joi"); //for server side schema validation
+const { listingSchema } = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+//for hopscotch
+app.use(cors());
 //for parsing data in reuests
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 //to use static files like css for images
@@ -34,6 +43,67 @@ app.get("/", (req, res) => {
   res.send("Hollaaa");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  // Check for validation errors
+  if (error) {
+    throw new expressError(400, error);
+  } else {
+    next();
+  }
+}
+
+//listing Route
+app.get("/listing", wrapAsync(async (req, res) => {
+  const allListings = await listing.find({});
+  res.render("listings/index", { allListings });
+}));
+
+//add new listing route
+app.get("/listing/new", (req, res) => {
+  res.render("listings/new");
+});
+
+app.post("/listing",validateListing, wrapAsync(async (req, res,next) => {
+
+  const newlisting = new listing(req.body.listing);
+  await newlisting.save();
+  res.redirect("/listing");
+}));
+
+//edit route to serve form
+app.get("/listing/:id/edit", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listingDetails = await listing.findById(id);
+  res.render("listings/edit", { listingDetails });
+}));
+
+//update route to update in db and redirect to show details
+app.put("/listing/:id", wrapAsync(async (req, res) => {
+  if (!req.body || !req.body.listing) {
+    throw new expressError(400, "Send valid data.");
+  }
+  let { id } = req.params;
+  await listing.findByIdAndUpdate(id, { ...req.body.listing });
+  console.log(req.body.listing.image);
+  res.redirect(`/listing/${id}`);
+}));
+
+//delete route
+app.delete("/listing/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listing");
+}));
+
+//Show details Route
+app.get("/listing/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listingDetails = await listing.findById(id);
+  res.render("listings/show", { listingDetails });
+}));
+
 //testing models
 // app.get("/test",async (req,res)=>{
 //     let sampleListing= new listing({
@@ -50,52 +120,19 @@ app.get("/", (req, res) => {
 //    res.send("Success");
 // });
 
-//listing Route
-app.get("/listing", async (req, res) => {
-  const allListings = await listing.find({});
-  res.render("./listings/index.ejs", { allListings });
+// catches errors with other routes that are in this code similar to app.all("*" res, next) => {
+//     next(new expressError(404, "Page not found!"));
+// });)
+
+app.use((req, res, next) => {
+  next(new expressError(404, "Page not found!"));
 });
 
-//add new listing route
-app.get("/listing/new", (req, res) => {
-  res.render("./listings/new.ejs");
-});
-
-app.post("/listing", (req, res) => {
-  const newlisting = new listing(req.body.listing);
-  newlisting.save();
-  res.redirect("/listing");
-});
-
-//edit route to serve form
-app.get("/listing/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listingDetails = await listing.findById(id);
-  res.render("./listings/edit.ejs", { listingDetails });
-});
-
-//update route to update in db and redirect to show details
-app.put("/listing/:id", async (req, res) => {
-  console.log("Holla");
-  let { id } = req.params;
-  await listing.findByIdAndUpdate(id, { ...req.body.listing });
-  console.log(req.body.listing.image);
-  res.redirect(`/listing/${id}`);
-});
-
-//delete route
-app.delete("/listing/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listing");
-});
-
-//Show details Route
-app.get("/listing/:id", async (req, res) => {
-  let { id } = req.params;
-  const listingDetails = await listing.findById(id);
-  res.render("./listings/show.ejs", { listingDetails });
+//custom Error handler
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong." } = err;
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error", { message });
 });
 
 app.listen("8080", () => {
